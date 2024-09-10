af9edc31643e89aa38116823ea55bcb29e42860a
Matteo Pita <ST10024454@vcconnect.edu.za>
2024-09-10 14:43:11 +0200

    Home page frontend done
    error in backend

--- app/src/main/java/com/example/alleysway/MainActivity.kt
+++ app/src/main/java/com/example/alleysway/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.alleysway
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,13 +16,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val createAccountTextView = findViewById<TextView>(R.id.text_signup)
+        val btnlogin = findViewById<ImageView>(R.id.image_rectangle3)
 
         createAccountTextView.setOnClickListener {
             navigateToRegisterPage()
         }
-    }
 
+        btnlogin.setOnClickListener {
+            login()
+        }
+    }
         private fun navigateToRegisterPage() {
             startActivity(Intent(this, Register_page::class.java))
+        }
+
+        private fun login() {
+        startActivity(Intent(this, HomePage::class.java))
         }
     }
