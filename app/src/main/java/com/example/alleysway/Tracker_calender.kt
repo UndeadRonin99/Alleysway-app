a8b4d22de4064607c8a0db1bf043915e4de0747d
Shira Bome <st10043352@vcconnect.edu.za>
2024-10-09 15:09:11 +0200

    Tracker_calender

---
+++ app/src/main/java/com/example/alleysway/Tracker_calender.kt
@@ -0,0 +1,20 @@
+package com.example.alleysway
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Tracker_calender : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tracker_calender)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
