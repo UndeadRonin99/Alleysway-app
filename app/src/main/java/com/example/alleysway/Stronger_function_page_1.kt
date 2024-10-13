ba59b1d9e044da98faaae4fb2d6598eb1af238eb
Shira Bome <st10043352@vcconnect.edu.za>
2024-10-13 15:25:45 +0200

    trying to recreate the stronger app

---
+++ app/src/main/java/com/example/alleysway/Stronger_function_page_1.kt
@@ -0,0 +1,20 @@
+package com.example.alleysway
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Stronger_function_page_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_stronger_function_page1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
